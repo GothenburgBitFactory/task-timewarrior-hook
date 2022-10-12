28ef740ddc3c4710230dceb3336b1d704760641f
Thomas Lauf <thomas.lauf@tngtech.com>
2022-10-12 22:43:50 +0200

    Adapt e2e test setup

--- test/test_on-modify_e2e.py
+++ test/test_on-modify_e2e.py
@@ -25,7 +25,6 @@
 # https://www.opensource.org/licenses/mit-license.php
 #
 ###############################################################################
-import os.path
 
 from basetest import Timew, Task, TestCase
 
@@ -37,50 +36,42 @@ class TestOnModifyHookScript(TestCase):
         self.timew.reset(keep_config=True)
 
         self.task = Task(datadir="/root/.task", taskrc="/root/.taskrc")
-        self.task.reset(keep_config=True)
-
-        with open(os.path.join(os.path.dirname(__file__), "..", "on-modify.py"), "r") as f:
-            self.task.hooks.clear()
-            self.task.hooks.add("on-modify", f.read(), False)
-
-        self.task.deactivate_hooks()
+        self.task.reset(keep_config=True, keep_hooks=True)
 
     def test_hook_should_process_annotate(self):
         """on-modify hook should process 'task annotate'"""
+        self.task.deactivate_hooks()
         self.task("add Foo")
         self.task("start 1")
         self.timew("start 10min ago Foo")
-
         self.task.activate_hooks()
 
         self.task("1 annotate Annotation")
 
         j = self.timew.export()
-
         self.assertEqual(len(j), 1)
         self.assertOpenInterval(j[0], expectedTags=["Foo"], expectedAnnotation="Annotation")
 
     def test_hook_should_process_append(self):
         """on-modify hook should process 'task append'"""
+        self.task.deactivate_hooks()
         self.task("add Foo")
         self.task("1 start")
         self.timew("start 10min ago Foo")
-
         self.task.activate_hooks()
 
         self.task("1 append Bar")
 
         j = self.timew.export()
-
         self.assertEqual(len(j), 1)
         self.assertOpenInterval(j[0], expectedTags=["Foo Bar"])
 
     def test_hook_should_process_delete(self):
         """on-modify hook should process 'task delete'"""
+        self.task.deactivate_hooks()
         self.task("add Foo")
         self.task("start 1")
         self.timew("start 10min ago Foo")
-
         self.task.activate_hooks()
 
         self.task("rc.confirmation=off delete 1")
@@ -91,13 +82,12 @@ class TestOnModifyHookScript(TestCase):
 
     def test_hook_should_process_denotate(self):
         """on-modify hook should process 'task denotate'"""
+        self.task.deactivate_hooks()
         self.task("add Foo")
         self.task("1 start")
         self.task("1 annotate Annotation")
-
         self.timew("start 10min ago Foo")
         self.timew("annotate @1 Annotation")
-
         self.task.activate_hooks()
 
         self.task("1 denotate")
@@ -108,10 +98,10 @@ class TestOnModifyHookScript(TestCase):
 
     def test_hook_should_process_done(self):
         """on-modify hook should process 'task done'"""
+        self.task.deactivate_hooks()
         self.task("add Foo")
         self.task("1 start")
         self.timew("start 10min ago Foo")
-
         self.task.activate_hooks()
 
         self.task("1 done")
@@ -122,10 +112,10 @@ class TestOnModifyHookScript(TestCase):
 
     def test_hook_should_process_modify_description(self):
         """on-modify hook should process 'task modify' for changing description"""
+        self.task.deactivate_hooks()
         self.task("add Foo")
         self.task("1 start")
         self.timew("start 10min ago Foo")
-
         self.task.activate_hooks()
 
         self.task("1 modify /Foo/Bar/")
@@ -136,11 +126,11 @@ class TestOnModifyHookScript(TestCase):
 
     def test_hook_should_process_modify_tags(self):
         """on-modify hook should process 'task modify' for changing tags"""
+        self.task.deactivate_hooks()
         self.task("add Foo")
         self.task("1 start")
         self.task("1 modify +Bar +Tag")
         self.timew("start 10min ago Foo Tag Bar")
-
         self.task.activate_hooks()
 
         self.task("1 modify -Bar +Baz")
@@ -151,11 +141,10 @@ class TestOnModifyHookScript(TestCase):
 
     def test_hook_should_process_modify_project(self):
         """on-modify hook should process 'task modify' for changing project"""
+        self.task.deactivate_hooks()
         self.task("add Foo project:dummy")
         self.task("1 start")
-
         self.timew("start 10min ago Foo dummy")
-
         self.task.activate_hooks()
 
         self.task("1 modify project:test")
@@ -166,11 +155,10 @@ class TestOnModifyHookScript(TestCase):
 
     def test_hook_should_process_prepend(self):
         """on-modify hook should process 'task prepend'"""
+        self.task.deactivate_hooks()
         self.task("add Foo")
         self.task("1 start")
-
         self.timew("start 10min ago Foo")
-
         self.task.activate_hooks()
 
         self.task("1 prepend 'Prefix '")
@@ -181,8 +169,8 @@ class TestOnModifyHookScript(TestCase):
 
     def test_hook_should_process_start(self):
         """on-modify hook should process 'task start'"""
+        self.task.deactivate_hooks()
         self.task("add Foo")
-
         self.task.activate_hooks()
 
         self.task("1 start")
@@ -193,11 +181,10 @@ class TestOnModifyHookScript(TestCase):
 
     def test_hook_should_process_stop(self):
         """on-modify hook should process 'task stop'"""
+        self.task.deactivate_hooks()
         self.task("add Foo")
         self.task("1 start")
-
         self.timew("start 10min ago Foo")
-
         self.task.activate_hooks()
 
         self.task("1 stop")
